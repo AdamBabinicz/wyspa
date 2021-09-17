0dac7287199a1484bc4cde8b95a079f55dcc659e
babinicz <babinicz@onet.pl>
2021-09-17 19:46:55 +0200

    zm

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -12,11 +12,11 @@ const Characters = () => {
       <div className="characters-specialization">
         <h3>Pan Młody</h3>
         <p>
-          Lucjan Rydel był młodopolskim poetą. W 1900 roku ożenił się z Jadwigą
-          Mikołajczykówną, siostrą Anny Tetmajerowej. Wesele odbyło się w dworku
-          Tetmajerów, w podkrakowskich Bronowicach. Pan Młody w „Weselu” jest
-          sportretowany ironicznie, popełnia liczne gafy, np. zdejmuje buty,
-          chcąc zaakcentować swoją chłopskość.
+          <b>Lucjan Rydel</b> był młodopolskim poetą. W 1900 roku ożenił się z
+          <b> Jadwigą Mikołajczykówną</b>, siostrą Anny Tetmajerowej. Wesele
+          odbyło się w dworku Tetmajerów, w podkrakowskich <b>Bronowicach</b>.
+          Pan Młody w „Weselu” jest sportretowany ironicznie, popełnia liczne
+          gafy, np. zdejmuje buty, chcąc zaakcentować swoją chłopskość.
         </p>
         <br />
         <h3>Panna Młoda</h3>
@@ -28,54 +28,55 @@ const Characters = () => {
         <br />
         <h3>Dziennikarz</h3>
         <p>
-          Rudolf Starzewski był redaktorem krakowskiego „Czasu”. W dramacie
-          Dziennikarz to człowiek inteligentny, o artystycznej naturze, ale
-          jednocześnie zachowawczy i pogrążony w apatii.
+          <b>Rudolf Starzewski</b> był redaktorem krakowskiego „Czasu”. W
+          dramacie <b>Dziennikarz</b> to człowiek inteligentny, o artystycznej
+          naturze, ale jednocześnie zachowawczy i pogrążony w apatii.
         </p>
         <br />
         <h3>Poeta</h3>
         <p>
-          Kazimierz Tetmajer, przyrodni brat Włodzimierza, był słynnym
+          <b>Kazimierz Tetmajer</b>, przyrodni brat Włodzimierza, był słynnym
           młodopolskim poetą, a także autorem dramatów i powieści. W „Weselu”
-          podkreśla się skłonność Poety do romansów i jego dekadencką naturę.
+          podkreśla się skłonność <b>Poety</b> do romansów i jego dekadencką
+          naturę.
         </p>
         <br />
         <h3>Radczyni</h3>
         <p>
-          Antonina Domańska była żoną profesora UJ, a także pisarką. Ciotka
-          Lucjana Rydla w dramacie prezentuje niechęć do młodopolskiej
+          <b>Antonina Domańska</b> była żoną profesora UJ, a także pisarką.
+          Ciotka Lucjana Rydla w dramacie prezentuje niechęć do młodopolskiej
           chłopomanii.
         </p>
         <br />
         <h3>Gospodarz</h3>
         <p>
-          Tetmajer był młodopolskim malarzem, pochodził z rodziny
+          <b>Tetmajer</b> był młodopolskim malarzem, pochodził z rodziny
           inteligenckiej. W 1890 roku ożenił się z chłopką, Anną
           Mikołajczykówną. Związek ów uchodził za mezalians, którego początkowo
           nie akceptowała ani rodzina, ani środowisko malarza. Małżeństwo było
           jednak udane, a Tetmajerowie z czasem dorobili się własnego dworu i
           cieszyli się szacunkiem. W dramacie Gospodarz jest wcieleniem
-          polskości, oddaje jednak nieopatrznie złoty róg Jaśkowi.
+          polskości, oddaje jednak nieopatrznie złoty róg <b>Jaśkowi</b>.
         </p>
         <br />
         <h3>Gospodyni (Anna Tetmajerowa)</h3>
         <p>
-          Anna Tetmajerowa, z domu Mikołajczykówna, z pochodzenia chłopka,
-          wyszła za Włodzimierza Tetmajera w 1890 roku. W dramacie wzór matki i
-          żony, każe schować podkowę.
+          <b>Anna Tetmajerowa</b>, z domu Mikołajczykówna, z pochodzenia
+          chłopka, wyszła za Włodzimierza Tetmajera w 1890 roku. W dramacie wzór
+          matki i żony, każe schować podkowę.
         </p>
         <br />
         <h3>Rachela</h3>
         <p>
-          Pepa Singer była córką bronowickiego karczmarza, Żydówką. W dramacie
-          Rachela jest postacią fascynującą i tajemniczą, poetką i znawczynią
-          sztuki.
+          <b>Pepa Singer</b> była córką bronowickiego karczmarza, Żydówką. W
+          dramacie <b>Rachela</b> jest postacią fascynującą i tajemniczą, poetką
+          i znawczynią sztuki.
         </p>
         <br />
         <h3>Czepiec (Błażej Czepiec)</h3>
         <p>
-          Czepiec był wujem Panny Młodej. W Bronowicach był pisarzem gminnym.
-          Podczas wesela wszczyna bójkę i upija się. Jest krytykiem
+          <b>Czepiec</b> był wujem Panny Młodej. W Bronowicach był pisarzem
+          gminnym. Podczas wesela wszczyna bójkę i upija się. Jest krytykiem
           mieszczańskich wad, ale jednocześnie patriotą.
         </p>
         <br />
@@ -87,10 +88,10 @@ const Characters = () => {
         <br />
         <h3>Marysia (Marysia Mikołajczykówna)</h3>
         <p>
-          Marysia była siostrą Panny Młodej, jej narzeczony (malarz Ludwik de
-          Levaux) zmarł za granicą na gruźlicę. Marysia wyszła potem za
-          Wojtusia, który również zmarł. W dramacie ukazuje się jej dawny
-          ukochany.
+          <b>Marysia</b> była siostrą Panny Młodej, jej narzeczony (malarz
+          Ludwik de Levaux) zmarł za granicą na gruźlicę. Marysia wyszła potem
+          za <b>Wojtusia</b>, który również zmarł. W dramacie ukazuje się jej
+          dawny ukochany.
         </p>
         <br />
         <h3>Nos (Tadeusz Nosowski lub Stanisław Czajkowski)</h3>
@@ -102,16 +103,16 @@ const Characters = () => {
         <br />
         <h3>Zosia</h3>
         <p>
-          Zofia Pareńska, córka znakomitego lekarza, profesora Uniwersytetu
-          Jagiellońskiego i prymariusza szpitala św. Łazarza; później wyszła za
-          mąż za Tadeusza Boya-Żeleńskiego.
+          <b>Zofia Pareńska</b>, córka znakomitego lekarza, profesora
+          Uniwersytetu Jagiellońskiego i prymariusza szpitala św. Łazarza;
+          później wyszła za mąż za <b>Tadeusza Boya-Żeleńskiego</b>.
         </p>
         <br />
         <h3>Haneczka</h3>
         <p>
-          Hana Rydlówna, siostra Lucjana Rydla. Charakter wszystkich trzech
-          dziewcząt oraz ich sposób mówienia został zachowany w dramacie bardzo
-          wiernie.
+          <b>Hana Rydlówna</b>, siostra Lucjana Rydla. Charakter wszystkich
+          trzech dziewcząt oraz ich sposób mówienia został zachowany w dramacie
+          bardzo wiernie.
         </p>
         <br />
         <p>
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -15,122 +15,137 @@ const Summary = () => {
           Jest listopadowa noc roku tysiąc dziewięćsetnego. Akcja dramatu
           rozgrywa się w izbie wiejskiej chaty, w której słychać dźwięki
           weselnej muzyki i gwar toczących się za ścianą rozmów. W scenie
-          pierwszej odbywa się rozmowa Dziennikarza z Czepcem, który stara się
-          zmusić przybysza z miasta do dostrzeżenia w chłopach ludzi godnych
-          uwagi, z których wywodził się przecież Głowacki. Według niego chłopi
-          są ciekawi wiadomości ze świata i każdego dnia czytają gazety.
-          Dziennikarz jest znudzony polityką i miejskim życiem. Pragnie
-          rozkoszować się spokojem wsi.
+          pierwszej odbywa się rozmowa <b>Dziennikarza</b> z <b>Czepcem</b>,
+          który stara się zmusić przybysza z miasta do dostrzeżenia w chłopach
+          ludzi godnych uwagi, z których wywodził się przecież <b>Głowacki</b>.
+          Według niego chłopi są ciekawi wiadomości ze świata i każdego dnia
+          czytają gazety. <b>Dziennikarz</b> jest znudzony polityką i miejskim
+          życiem. Pragnie rozkoszować się spokojem wsi.
         </p>
         <br />
         <p>
-          Kolejne sceny przybliżają postacie Zosi i Haneczki – młodych panien,
-          pełnych radości życia i skorych do tańców; Radczyni – kobiety
-          wyniosłej, pobłażliwej dla ludu, lecz jednocześnie przekonanej o swej
-          wyższości; Jaśka i Kaspra – wesołych chłopaków ze wsi; starej Kliminy,
-          lubiącej zabawę i chętnej do swatania młodych; Pana Młodego,
+          Kolejne sceny przybliżają postacie <b>Zosi</b> i <b>Haneczki</b> –
+          młodych panien, pełnych radości życia i skorych do tańców;
+          <b> Radczyni</b> – kobiety wyniosłej, pobłażliwej dla ludu, lecz
+          jednocześnie przekonanej o swej wyższości; <b>Jaśka</b> i
+          <b> Kaspra</b>– wesołych chłopaków ze wsi; starej <b>Kliminy</b>,
+          lubiącej zabawę i chętnej do swatania młodych; <b>Pana Młodego</b>,
           cieszącego się własnym szczęściem i zachwyconego urodą wybranki serca
-          oraz Panny Młodej – dziewczyny rozsądnej, naiwnej i prostolinijnej.
-          Poeta, znużony powodzeniem u kobiet oraz pełen niewiary w życie i
-          sztukę, flirtuje z Maryną. W pewnym momencie zjawia się Żyd,
-          dzierżawca pobliskiej karczmy. W rozmowie z Panem Młodym uwidacznia
-          różnice między poszczególnymi grupami społecznymi, a także zarzuca
-          młodzieńcowi, iż zanadto zachwyca się wiejskimi zwyczajami, a przecież
-          następnego dnia zrzuci chłopski strój.
+          oraz
+          <b> Panny Młodej</b> – dziewczyny rozsądnej, naiwnej i prostolinijnej.
+          <b> Poeta</b>, znużony powodzeniem u kobiet oraz pełen niewiary w
+          życie i sztukę, flirtuje z <b>Maryną</b>. W pewnym momencie zjawia się
+          <b> Żyd</b>, dzierżawca pobliskiej karczmy. W rozmowie z
+          <b> Panem Młodym</b> uwidacznia różnice między poszczególnymi grupami
+          społecznymi, a także zarzuca młodzieńcowi, iż zanadto zachwyca się
+          wiejskimi zwyczajami, a przecież następnego dnia zrzuci chłopski
+          strój.
         </p>
         <br />
         <p>
-          Pan Młody nie kryje swojego zauroczenia żoną – dziewczyną z prostego
-          ludu, przy której inne panny wydają mu się przeciętne. Do izby
-          weselnej wchodzi córka Żyda, Rachela. Pan Młody jest zachwycony
-          wiejskim życiem, czuje się tu zdrowszym. Jest przekonany, że pewnego
-          dnia napisze poemat o życiu na wsi. Obecność Racheli, rozmiłowanej w
-          poezji i niezwykle uduchowionej, sprawia, iż goście weselni zaczynają
-          rozmawiać o sztuce i twórczych tęsknotach. Poeta pragnie napisać
-          dramat, rozmawia z Gospodarzem o roli artysty. Ich dialog przerywa
-          pojawienie się Czepca i Ojca. Poeta określa siebie mianem „żurawca”,
+          <b>Pan Młody</b> nie kryje swojego zauroczenia żoną – dziewczyną z
+          prostego ludu, przy której inne panny wydają mu się przeciętne. Do
+          izby weselnej wchodzi córka <b>Żyda</b>, <b>Rachela</b>.
+          <b> Pan Młody </b>
+          jest zachwycony wiejskim życiem, czuje się tu zdrowszym. Jest
+          przekonany, że pewnego dnia napisze poemat o życiu na wsi. Obecność
+          <b> Racheli</b>, rozmiłowanej w poezji i niezwykle uduchowionej,
+          sprawia, iż goście weselni zaczynają rozmawiać o sztuce i twórczych
+          tęsknotach. <b>Poeta</b> pragnie napisać dramat, rozmawia z
+          <b> Gospodarzem</b> o roli artysty. Ich dialog przerywa pojawienie się
+          <b> Czepca i Ojca</b>. <b>Poeta</b> określa siebie mianem „żurawca”,
           który jedynie przelotem bywa w kraju. Chłopi uznają, że mężczyzna nie
-          jest szczęśliwy, a Czepiec radzi, aby poślubił dziewczynę z ludu,
-          która da mu dużo szczęścia.
+          jest szczęśliwy, a <b>Czepiec</b> radzi, aby poślubił dziewczynę z
+          ludu, która da mu dużo szczęścia.
         </p>
         <br />
         <p>
-          Następuje kłótnia o dług między Czepcem a Żydem, do której przyłącza
-          się Ksiądz, upominając się o należność za dzierżawę karczmy. Gospodarz
-          przypomina im krwawą rzeź, jakiej dokonali przed laty na szlachcie
-          chłopi. Pan Młody wyznaje, że zna to wydarzenie z opowieści, lecz nie
-          chce o nim myśleć, aby nie niszczyć swoich wyobrażeń o polskiej wsi.
-          Dodaje, że szlachta zapomniała już o krzywdzie i teraz brata się z
-          ludem, strojąc się w pawie pióra. W końcowej scenie Poeta ulega
-          sugestii Racheli i prosi nowożeńców, aby zaprosili na wesele stojącego
-          w sadzie chochoła. Młoda para, śmiejąc się i żartując, zaprasza razem
-          z Chochołem każdego, kogo zechce przyprowadzić, by wespół z nimi
-          cieszył się ich szczęściem.
+          Następuje kłótnia o dług między <b>Czepcem</b> a <b>Żydem</b>, do
+          której przyłącza się <b>Ksiądz</b>, upominając się o należność za
+          dzierżawę karczmy. <b>Gospodarz</b> przypomina im krwawą rzeź, jakiej
+          dokonali przed laty na szlachcie chłopi. <b>Pan Młody</b> wyznaje, że
+          zna to wydarzenie z opowieści, lecz nie chce o nim myśleć, aby nie
+          niszczyć swoich wyobrażeń o polskiej wsi. Dodaje, że szlachta
+          zapomniała już o krzywdzie i teraz brata się z ludem, strojąc się w
+          pawie pióra. W końcowej scenie <b>Poeta</b> ulega sugestii
+          <b> Racheli</b> i prosi nowożeńców, aby zaprosili na wesele stojącego
+          w sadzie <b>chochoła</b>. Młoda para, śmiejąc się i żartując, zaprasza
+          razem z <b>Chochołem</b> każdego, kogo zechce przyprowadzić, by wespół
+          z nimi cieszył się ich szczęściem.
         </p>
         <br />
         <h3>Akt II</h3>
         <p>
-          W pustej izbie siedzi Isia, córka Gospodarzy, który usypia młodsze
-          dziecko. Wybija północ i do pomieszczenia wchodzi Chochoł. Dziewczyna
-          stara się przegnać niespodziewanego gościa. Po chwili do izby wchodzi
-          Marysia z mężem Wojtkiem. Po ich rozmowie kobiecie ukazuje się Widmo,
-          w którym rozpoznaje zmarłego przed laty narzeczonego. Dziennikarzowi
-          ukazuje się Stańczyk. Spotkanie to burzy spokój mężczyzny,
-          przypominając mu o niewoli narodowej i niemocy podjęcia walki
-          narodowowyzwoleńczej. Do Poety przychodzi Rycerz, symbolizujący
-          minioną chwałę i siłę narodu. Przed Panem Młodym staje zjawa Hetmana
-          Branickiego, uważanego za symbol narodowej zdrady i szlacheckiego
-          warcholstwa. Dziadowi ukazuje się Upiór Szeli – chłopskiego przywódcy
-          krwawych rzezi galicyjskich z roku 1846.
+          W pustej izbie siedzi <b>Isia</b>, córka <b>Gospodarzy</b>, który
+          usypia młodsze dziecko. Wybija północ i do pomieszczenia wchodzi
+          <b> Chochoł</b>. Dziewczyna stara się przegnać niespodziewanego
+          gościa. Po chwili do izby wchodzi <b>Marysia</b> z mężem
+          <b> Wojtkiem</b>. Po ich rozmowie kobiecie ukazuje się <b>Widmo</b>, w
+          którym rozpoznaje zmarłego przed laty narzeczonego.
+          <b> Dziennikarzowi</b> ukazuje się
+          <b> Stańczyk</b>. Spotkanie to burzy spokój mężczyzny, przypominając
+          mu o niewoli narodowej i niemocy podjęcia walki narodowowyzwoleńczej.
+          Do
+          <b> Poety</b> przychodzi <b>Rycerz</b>, symbolizujący minioną chwałę i
+          siłę narodu. Przed <b>Panem Młodym</b> staje
+          <b> zjawa Hetmana Branickiego</b>, uważanego za symbol narodowej
+          zdrady i szlacheckiego warcholstwa. <b>Dziadowi</b> ukazuje się
+          <b> Upiór Szeli</b> – chłopskiego przywódcy krwawych rzezi
+          galicyjskich z roku 1846.
         </p>
         <br />
         <p>
           Rozmowy weselników z przybyszami z zaświatów odkrywają ich marzenia,
           lęki oraz przypominają o bolesnych wydarzeniach z przeszłości. Na
-          dziedziniec przed chatę wjeżdża Wernyhora, który przybywa do
-          Gospodarza z rozkazem, aby rozesłać wici, wzywające lud do powstania.
-          Gospodarz otrzymuje odpowiednie wskazówki i złoty róg, którego dźwięk
-          ma w odpowiedniej chwili rozbudzić w sercach ludzi wolę walki.
-          Zmęczony mężczyzna przekazuje róg Jaśkowi, nakazując mu wezwanie
-          wszystkich chłopów, aby uzbrojeni stawili się o świcie przed kaplicą.
-          Jasiek wybiega z izby, a Gospodarz dostaje od Staszka znalezioną przez
-          niego złotą podkowę, która jest dla niego dowodem wizyty Wernyhory.
+          dziedziniec przed chatę wjeżdża <b>Wernyhora</b>, który przybywa do
+          <b>Gospodarza</b> z rozkazem, aby rozesłać wici, wzywające lud do
+          powstania. <b>Gospodarz</b> otrzymuje odpowiednie wskazówki i
+          <b> złoty róg</b>, którego dźwięk ma w odpowiedniej chwili rozbudzić w
+          sercach ludzi wolę walki. Zmęczony mężczyzna przekazuje róg
+          <b> Jaśkowi</b>, nakazując mu wezwanie wszystkich chłopów, aby
+          uzbrojeni stawili się o świcie przed kaplicą. <b>Jasiek</b> wybiega z
+          izby, a <b>Gospodarz</b> dostaje od <b>Staszka</b> znalezioną przez
+          niego <b>złotą podkowę</b>, która jest dla niego dowodem wizyty
+          <b> Wernyhory</b>.
         </p>
         <br />
         <h3>Akt III</h3>
         <p>
           Zbliża się ranek, goście weselni są już zmęczeni całonocną zabawą i
-          tańcami. W izbie zjawia się pijany Nos, szukający zapomnienia w
-          alkoholu. Panna Młoda opowiada Poecie sen, w którym diabły wiozły ją w
-          złotej karecie do Polski. Dziewczyna dopytuje się, gdzie jest Polska.
-          Mężczyzna wyjaśnia, że nie znajdzie tego kraju, choćby szukała po
-          całym świecie, ale odnajdzie go w swoim sercu. Do Czepca podchodzi
-          Kuba i wspomina o nocnej wizycie Wernyhory i złotej podkowie, którą
-          Gospodyni schowała w skrzyni. Czepiec, który nie wierzy w opowieść
-          parobka, stara się obudzić Gospodarza. Kiedy to mu się nie udaje,
-          sprowadza uzbrojonych chłopów.
+          tańcami. W izbie zjawia się pijany <b>Nos</b>, szukający zapomnienia w
+          alkoholu. <b>Panna Młoda</b> opowiada <b>Poecie</b> sen, w którym
+          diabły wiozły ją w złotej karecie do Polski. Dziewczyna dopytuje się,
+          gdzie jest Polska. Mężczyzna wyjaśnia, że nie znajdzie tego kraju,
+          choćby szukała po całym świecie, ale odnajdzie go w swoim sercu. Do
+          <b> Czepca</b> podchodzi <b>Kuba</b> i wspomina o nocnej wizycie
+          <b> Wernyhory</b> i <b>złotej podkowie</b>, którą <b>Gospodyni </b>
+          schowała w skrzyni. <b>Czepiec</b>, który nie wierzy w opowieść
+          parobka, stara się obudzić <b>Gospodarza</b>. Kiedy to mu się nie
+          udaje, sprowadza uzbrojonych chłopów.
         </p>
         <br />
         <p>
-          Przebudzony Gospodarz nie pamięta nocnych wydarzeń. Rozgniewany
-          Czepiec grozi, że jeśli panowie nie przyłączą się do nich, to chłopi
-          rozprawią się z nimi za pomocą kos. Pan Młody nie słucha Czepca i
-          zachwyca się pięknem przyrody o świcie. Nie zwraca też uwagi na
-          ostrzeżenie chłopa, że zbliża się pora, w której powinni oczekiwać
-          obiecanego im znaku. Gospodarz nagle przypomina sobie swoją rozmowę z
-          Wernyhorą. Wszyscy zebrani odczuwają podniecenie, słyszą wrzawę i
-          dostrzegają dziwne znaki na niebie. Oczekują na odpowiedni znak.
+          Przebudzony <b>Gospodarz</b> nie pamięta nocnych wydarzeń. Rozgniewany
+          <b> Czepiec</b> grozi, że jeśli panowie nie przyłączą się do nich, to
+          chłopi rozprawią się z nimi za pomocą kos. <b>Pan Młody</b> nie słucha
+          <b> Czepca</b> i zachwyca się pięknem przyrody o świcie. Nie zwraca
+          też uwagi na ostrzeżenie chłopa, że zbliża się pora, w której powinni
+          oczekiwać obiecanego im znaku. <b>Gospodarz</b> nagle przypomina sobie
+          swoją rozmowę z <b>Wernyhorą</b>. Wszyscy zebrani odczuwają
+          podniecenie, słyszą wrzawę i dostrzegają dziwne znaki na niebie.
+          Oczekują na odpowiedni znak.
         </p>
         <br />
         <p>
           W oddali słychać tętent końskich kopyt. Zebrani w izbie ludzie
           nieruchomieją, trzymając w dłoniach kosy i szable, zdjęte ze ścian. Do
-          pomieszczenia wbiega Jasiek. Zaskoczony niezwykłym widokiem,
-          przypomina sobie, że miał zadąć w złoty róg. Z przerażeniem stwierdza,
-          że zgubił powierzony mu przez Gospodarza przedmiot. Za chłopakiem do
-          izby wchodzi Chochoł. Uświadamia Jaśkowi, iż zgubił róg w momencie,
-          kiedy schylił się po czapkę z pawimi piórami. Jasiek bezskutecznie
-          usiłuje zbudzić ludzi, zebranych w izbie. Chochoł nakazuje mu, aby
+          pomieszczenia wbiega <b>Jasiek</b>. Zaskoczony niezwykłym widokiem,
+          przypomina sobie, że miał zadąć w <b>złoty róg</b>. Z przerażeniem
+          stwierdza, że zgubił powierzony mu przez <b>Gospodarza</b> przedmiot.
+          Za chłopakiem do izby wchodzi <b>Chochoł</b>. Uświadamia
+          <b> Jaśkowi</b>, iż zgubił róg w momencie, kiedy schylił się po
+          <b> czapkę z pawimi piórami</b>. <b>Jasiek</b> bezskutecznie usiłuje
+          zbudzić ludzi, zebranych w izbie. <b>Chochoł</b> nakazuje mu, aby
           wyjął z ich rąk szable i kosy. Zaczyna grać na skrzypcach, a ludzie
           tańczą w rytm chocholego śpiewu.
         </p>
