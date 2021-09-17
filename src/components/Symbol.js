469000a0e22b00cbb028f258f1f23ea8bacff727
babinicz <babinicz@onet.pl>
2021-09-17 20:05:09 +0200

    zm

--- src/components/Symbol.js
+++ src/components/Symbol.js
@@ -14,48 +14,52 @@ const Symbol = () => {
         <p>
           Wyspiański w „Weselu” posługuje się wyrazistymi symbolami, które są
           nośnikami określonych znaczeń i funkcji. W dramacie odnajdziemy zatem
-          symboliczne postaci, działania oraz rekwizyty. Cały akt II „Wesela”
-          przybiera kształt swoistego seansu zjaw i duchów. Pojawiają się
-          wówczas tacy symboliczni bohaterowie, jak Widmo (symbol romantycznej
-          miłości), Stańczyk (symbol bezsilności), Rycerz (symbol marzeń o
-          wielkich czynach), Hetman (symbol zdrady), Jakub Szela (symbol rzezi
-          galicyjskiej) oraz Wernyhora (symbol narodowego odrodzenia).
+          <b> symboliczne postaci, działania oraz rekwizyty</b>. Cały akt II
+          „Wesela” przybiera kształt swoistego seansu zjaw i duchów. Pojawiają
+          się wówczas tacy symboliczni bohaterowie, jak{" "}
+          <b>
+            Widmo (symbol romantycznej miłości), Stańczyk (symbol bezsilności),
+            Rycerz (symbol marzeń o wielkich czynach), Hetman (symbol zdrady),
+            Jakub Szela (symbol rzezi galicyjskiej) oraz Wernyhora (symbol
+            narodowego odrodzenia)
+          </b>
+          .
         </p>
         <br />
         <p>
-          Symboliczna jest również czasoprzestrzeń utworu. Bronowicki dwór,
-          niczym Mickiewiczowskie Soplicowo, stanowi niejako miniaturę całej
-          Polski, w której współistnieją elementy chłopskie i szlacheckie. Ważną
-          funkcję pełnią wiszące na ścianach obrazy: „Wernyhora” oraz „Bitwa pod
-          Racławicami” Jana Matejki. Odnoszą się one do tych momentów polskiej
-          historii, w których chłopi i inteligencja jednoczyli się w obronie
-          ojczyzny.
+          Symboliczna jest również czasoprzestrzeń utworu.
+          <b> Bronowicki dwór</b>, niczym Mickiewiczowskie Soplicowo, stanowi
+          niejako miniaturę całej Polski, w której współistnieją elementy
+          chłopskie i szlacheckie. Ważną funkcję pełnią wiszące na ścianach
+          obrazy: <b>„Wernyhora” oraz „Bitwa pod Racławicami” Jana Matejki</b>.
+          Odnoszą się one do tych momentów polskiej historii, w których chłopi i
+          inteligencja jednoczyli się w obronie ojczyzny.
         </p>
         <br />
         <p>
-          Bardzo istotnym symbolem w dramacie jest złoty róg, który Wernyhora
-          ofiarowuje Gospodarzowi. Jego dźwięk ma przebudzić uśpione serca
-          rodaków i przygotować ich do walki o wolność. Gospodarz oddaje jednak
-          złoty róg Jaśkowi, a sam zasypia. Gest ów oznacza, że inteligencja nie
-          chce pełnić przewodniej roli i zrzeka się jej. Zgubienie złotego rogu
-          przez chłopa stanowi zaś symbol zaprzepaszczenia szansy na odzyskanie
-          wolności.
+          Bardzo istotnym symbolem w dramacie jest <b>złoty róg</b>, który
+          Wernyhora ofiarowuje Gospodarzowi. Jego dźwięk ma przebudzić uśpione
+          serca rodaków i przygotować ich do walki o wolność. Gospodarz oddaje
+          jednak złoty róg Jaśkowi, a sam zasypia. Gest ów oznacza, że
+          inteligencja nie chce pełnić przewodniej roli i zrzeka się jej.
+          Zgubienie złotego rogu przez chłopa stanowi zaś symbol
+          zaprzepaszczenia szansy na odzyskanie wolności.
         </p>
         <br />
         <p>
-          Ważna jest również czapka z pawimi piórami, po którą schyla się
+          Ważna jest również <b>czapka z pawimi piórami</b>, po którą schyla się
           Jasiek, gubiąc przy okazji złoty róg Wernyhory. Oznacza ona
           przywiązanie chłopstwa do wartości materialnych, które są dla nich
-          najważniejsze. Z kolei sznur zostający w rękach chłopca po zgubie
-          przypomina zaś o zniewoleniu Polaków.
+          najważniejsze. Z kolei <b>sznur</b> zostający w rękach chłopca po
+          zgubie przypomina zaś o zniewoleniu Polaków.
         </p>
         <br />
         <p>
-          Pozytywnym symbolem w „Weselu” jest natomiast chochoł jako snop
+          Pozytywnym symbolem w „Weselu” jest natomiast <b>chochoł</b> jako snop
           okrywający podczas zimy krzak róży. Można bowiem przypuszczać, że
           podobnie jak chochoł jest zrzucany na wiosnę, tak Polacy zrzucą
-          kajdany i odzyskają niepodległość. Z kolei chocholi korowód w końcowej
-          scenie dramatu stanowi symbol marazmu i zniewolenia polskiego
+          kajdany i odzyskają niepodległość. Z kolei <b>chocholi korowód</b> w
+          końcowej scenie dramatu stanowi symbol marazmu i zniewolenia polskiego
           społeczeństwa.
         </p>
         <br />
